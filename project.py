de9291a3281e6477274f05bcbaac61871b6b5d4e
Renata <[renatajarmova@gmail.com]>
2019-05-20 12:09:44 +0100

    Add new html files

--- project.py
+++ project.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html")
 
 
 app.run(debug=True)
