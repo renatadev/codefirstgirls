d86099a5d56f278a015dd95340f82fab2d0d87b1
Renata <[renatajarmova@gmail.com]>
2019-05-21 13:47:39 +0100

    Add user validation

--- project/forms.py
+++ project/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm  #import our forms without using html but using python classes and a flask extension
 from wtforms import StringField, PasswordField, SubmitField, BooleanField #for fields that are imported from wtf package
 from wtforms.validators import DataRequired, Length, Email, EqualTo #non empty validator, etc
+from project.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -13,6 +14,15 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
