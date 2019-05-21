ff301e86ca545a7079570a93ad77c4f5112fa82e
Renata <[renatajarmova@gmail.com]>
2019-05-21 12:39:24 +0100

    Fix typo in routes

--- project/routes.py
+++ project/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect
-from flaskblog import app
-from flaskblog.forms import RegistrationForm, LoginForm
-from flaskblog.models import User, Post
+from project import app
+from project.forms import RegistrationForm, LoginForm
+from project.models import User, Post
 
 
 posts = [
