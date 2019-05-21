16abe2bfc12444e9f39101ba59a8b6c4805b0c5e
Renata <[renatajarmova@gmail.com]>
2019-05-21 12:38:09 +0100

    Add imports to routes file

--- project/routes.py
+++ project/routes.py
@@ -1,3 +1,24 @@
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 #Decorators (routes)
 @app.route("/")
 @app.route("/home")
