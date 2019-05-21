b4cfe6c731440589e1276a5d71f72ff140427b6d
Renata <[renatajarmova@gmail.com]>
2019-05-21 11:45:16 +0100

    Try to add db file

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import Flask, render_template, request, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
+fromm database import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
