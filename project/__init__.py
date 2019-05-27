437e0332c5ef037c1839133926d91f6ca628b31e
Renata Jarmova <renatajarmova@jamess-mini2-5.home>
2019-05-27 14:17:33 +0100

    Add js and bootstrap files

--- project/__init__.py
+++ project/__init__.py
@@ -6,7 +6,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
